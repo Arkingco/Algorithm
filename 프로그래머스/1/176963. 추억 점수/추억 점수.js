821a0d39d944dd82f9c1c8991ee502640382d045
kipark <Arkingco@gmail.com>
2024-01-19 23:15:28 +0900

    [level 1] Title: 추억 점수, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,18 @@
+function solution(name, yearning, photo) {
+    var answer = [];
+    
+    var nameNumber = [];
+    
+    for (let i=0; i<name.length; ++i) 
+        nameNumber[name[i]] = yearning[i];
+    for (let i=0; i<photo.length; ++i) {
+        let sum = 0;
+        for (let j=0; j<photo[i].length; ++j) {
+            if(name.includes(photo[i][j])) {
+                sum += nameNumber[photo[i][j]];
+            }
+        }
+        answer.push(sum);
+    }
+    return answer;
+}
